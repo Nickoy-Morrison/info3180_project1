c78c54df96f0d229a3b97888250bfe7aa621f786
Nickoy Morrison <nickoy.morrison@mymona.uwi.edu>
2019-03-15 20:35:36 +0000

    finish system

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from subprocess import call
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "SuperSecretKey"
-#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://fmnibhaashbxuy:73b8e2e2485adfd45f57da653d63950b88fdcae12202a84f80c7f4c297e9e30a@ec2-23-23-222-184.compute-1.amazonaws.com:5432/d27ig8fpt4ch7r"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://bnffgxgcxgyobi:94154bed8bf0e06416ef2ccb2c4e1c73b936b229b5ac570b4edc3522ff48842b@ec2-184-73-153-64.compute-1.amazonaws.com:5432/d6urq5baj9736a"
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:password123@localhost/project1"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 app.config['UPLOAD_FOLDER'] = './app/static/photo'
